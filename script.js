9bac5f1bc429dc10fdf405e1cf2e84f02e931f6f
lukmanuthmanabubakr <123726979+lukmanuthmanabubakr@users.noreply.github.com>
2023-06-23 06:43:11 +0100

    last commit

--- script.js
+++ script.js
@@ -261,3 +261,9 @@ if(age >= 18) {
 }
 
 console.log(drink2);
+
+
+const bill = 275
+const tip = bill <= 300 && bill>=50 ? bill * 0.15 : bill * 0.2;
+
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value is ${bill + tip}`);
